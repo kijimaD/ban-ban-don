24834986c9235d325dc43f91033cac1966ef7451
kijima <norimaking777@gmail.com>
2020-05-25 09:14:59 +0900

    Add bounding box prototype

--- entities/components/character_graphics.rb
+++ entities/components/character_graphics.rb
@@ -1,4 +1,10 @@
 class CharacterGraphics < Component
+  DEBUG_COLORS = [
+    Gosu::Color::RED,
+    Gosu::Color::BLUE,
+    Gosu::Color::YELLOW,
+    Gosu::Color::WHITE
+  ]
   def initialize(game_object)
     super(game_object)
     @body = units.frame('tank1_body.png')
@@ -10,6 +16,20 @@ class CharacterGraphics < Component
     @shadow.draw_rot(x - 1, y - 1, 0, object.direction)
     @body.draw_rot(x, y, 1, object.direction)
     @gun.draw_rot(x, y, 2, object.gun_angle)
+    draw_bounding_box if $debug
+  end
+
+  def draw_bounding_box
+    $window.rotate(object.direction, x, y) do
+      w = @body.width
+      h = @body.height
+      $window.draw_quad(
+        x - w / 2, y - h / 2, Gosu::Color::RED,
+        x + w / 2, y - h / 2, Gosu::Color::RED,
+        x + w / 2, y + h / 2, Gosu::Color::RED,
+        x - w / 2, y + h / 2, Gosu::Color::RED,
+        100)
+    end
   end
 
   private
