982cc0d76f1dbdbae7da78d631b13a511c347c32
kijima <norimaking777@gmail.com>
2020-06-17 18:57:53 +0900

    Add radar(same)

--- entities/hud.rb
+++ entities/hud.rb
@@ -4,17 +4,20 @@ class HUD
     @object_pool = object_pool
     @character = character
     @ammo_display = AmmoDisplay.new(@object_pool, @character)
-    @speed_meter = SpeedMeter.new(@boject_pool, @character)
+    @speed_meter = SpeedMeter.new(@object_pool, @character)
+    @radar = Radar.new(@object_pool, @character)
   end
 
   def update
     @ammo_display.update
     @speed_meter.update
+    @radar.update
   end
 
   def draw
     @ammo_display.draw
     @speed_meter.draw
+    @radar.draw
   end
 
 end
